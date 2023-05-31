9b4d9732f1fb5a8b1f577e4df319497e6f0de59c
Haroune Hamenniche <91731030+haroune22@users.noreply.github.com>
2023-05-31 14:42:32 +0100

    Update Firebase.jsx

--- client/src/Firebase.jsx
+++ client/src/Firebase.jsx
@@ -1,13 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider} from "firebase/auth";
+//fireabse Config 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8fva8ZUOQFABHAalWbxEIhLFusk5gfc8",
-  authDomain: "fir-f4720.firebaseapp.com",
-  projectId: "fir-f4720",
-  storageBucket: "fir-f4720.appspot.com",
-  messagingSenderId: "431687956201",
-  appId: "1:431687956201:web:616fc527ef22ac59d6e278"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
